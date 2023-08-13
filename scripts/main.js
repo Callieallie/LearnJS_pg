86c045ef5faa9eaed9792fda5179a2efe8db73e8
Callieallie <callieacts@gmail.com>
2023-08-13 18:18:53 -0400

    Fresh project

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+// Created on August 13th, 2023 in the evening
+// Author: Caroline Wayne
+// Following tutorials provided by learnjavascript.online
+
+// -------- Basic Functions --------
+function sum(x, y) {
+    return x + y;
+}
