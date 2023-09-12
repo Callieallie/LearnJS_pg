722086bf0647bc5068a0195fafca9cf33b6db320
Callieallie <callieacts@gmail.com>
2023-09-12 15:34:32 -0400

    End of free portion LearnJS. Beginning ch. 8

---
+++ scripts/about.js
@@ -0,0 +1,3 @@
+// Created on August 12th, 2023 in the evening
+// Author: Caroline Wayne
+// Following tutorials provided by learnjavascript.online (begining in chapter 8 - Objects)
--- scripts/main.js
+++ scripts/main.js
@@ -103,6 +103,7 @@ function isPassing (grade) {
 }
 
 // -------- Arrays --------
+// JavaScript arrays are analogous to vectors in C++.
 const flowers = ["peony", "daisy", "rose", "petunia", "sunflower", "violet", "chrysanthimum", "lilac"];
 const plantColors = ["pink", "white", "red", "orange", "yellow", "purple", "blue", "perriwinkle"];
 
@@ -111,4 +112,89 @@ flowerListP.innerText = `The ${flowers[1]} is ${plantColors[1]}!
 And the length of the flowers array is ${flowers.length}.\n`;
 
 // The push method will return the new length of the array
-flowerListP.append(`The length of the flowers array is now ${flowers.push("poppy")}`);
+// There is no append method for strings in JavaScript. Use the concat method instead.
+// There is an append method for html nodes.
+flowerListP.innerText += `The length of the flowers array is now ${flowers.push("poppy")}`;
+
+// -------- Callbacks --------
+// "A callback is a function definition passed as an argument to another function."
+// Array Iteration using .foreach method.
+// Alternative to .foreach is a for-of/for-each loop
+const letterCount = [];
+flowers.forEach(function(flower) {
+    letterCount.push(flower.length);
+});
+
+// Adding to an existing string using concat method
+let currentFlowerIndex = 0;
+while (currentFlowerIndex < flowers.length) {
+    flowerListP.innerText = flowerListP.innerText.concat("\n", flowers[currentFlowerIndex] +
+    ": " + letterCount[currentFlowerIndex]);
+    currentFlowerIndex = currentFlowerIndex + 1;
+}
+
+// -------- Array Methods --------
+// .filter()
+// The .filter() method will ALWAYS return an array
+const numbers = [2, 94, 33, 7, 0, 51, 106];
+const numbersUnderFifty = numbers.filter(function(number) {
+    return number < 50;
+});
+
+const arrayMethodsP = document.getElementById("arrayMethods");
+arrayMethodsP.innerText = "Numbers under fifty: " + numbersUnderFifty;
+
+// .find()
+// The .find() method will return the first item that matches the condition specified
+// If no match is found, the .find() method will return as undefined
+const pNamedFlower = flowers.find(function(flower) {
+    return flower[0] === 'p';
+});
+
+arrayMethodsP.innerText += "\n" + pNamedFlower;
+
+// -------- More Array Methods --------
+// .map()
+// This is a special application of the map function called zipping
+const milesDrivenTrips = [306, 829, 252, 136, 724];
+const hoursPerTrips = [4, 8, 3, 1.4, 0.89, 10];
+
+const milesPerHour = milesDrivenTrips.map(function(milesDriven, index) {
+    return milesDriven / hoursPerTrips[index];
+});
+
+// .includes()
+// Returns true or false 
+const petNames = [Leopold, Beatrice, Guy, Charlotte];
+petNames.includes("Guy"); // true
+
+// .join()
+// The DOM converts arrays to strings and defaults to joining then with the comma
+// The join function alllows you to specify a "glue" besides the comma
+const trees = [spruce, maple, oak, birch, pine, willow];
+trees.join("; "); // "spruce; maple; oak; birch; pine; willow"
+
+// -------- Objects --------
+// Comparable to a struct: one variable that groups several 
+// In C++, an object is only ever an instance of a class
+// In JavaScript, an object can stand alone
+const boyfriend = {
+    name: "Scott",
+    age: 25,
+    employer: "Goog",
+    hairColor: "dirty blonde",
+    eyeColor: "ocean blue",
+    isHeAdorable: true,
+    sexyRide: "Honda C300F",
+    snores: true,
+    isAHose: false
+};
+
+
+
+// -------- EXERCISE IDEAS --------
+/*
+- Nail Salon: polish colors, nail shapes, prices
+- Smoothie Bowl Cafe: ingredients, toppings, nutrition powders, spoons
+- Horses: coat color, breed, age, name, owner, stall number, shoe size
+*/
