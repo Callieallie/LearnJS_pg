08a2a739846a0dd2795e9ffb6500826b6c3413b9
Callieallie <callieacts@gmail.com>
2023-09-12 16:18:03 -0400

    Created two horse-themed objects

--- scripts/about.js
+++ scripts/about.js
@@ -1,3 +1,39 @@
 // Created on August 12th, 2023 in the evening
 // Author: Caroline Wayne
 // Following tutorials provided by learnjavascript.online (begining in chapter 8 - Objects)
+
+// -------- Objects --------
+const horse1 = {
+    id: 1,
+    name: "Galeleo",
+    riders: ["Belle", "Kim"],
+    age: 3,
+    coatColor: "black",
+    stallNumber: 38,
+    jumping: true,
+    dressage: true,
+    polo: false
+}
+
+const horse2 = {
+    id: 2,
+    name: "Juniper",
+    riders: ["Chloe", "Royal"],
+    age: 4,
+    coatColor: "golden",
+    stallNumber: 22,
+    jumping: false,
+    dressage: true,
+    polo: false
+}
+
+
+// -------- EXERCISE IDEAS --------
+/*
+- Nail Salon: polish colors, nail shapes, prices
+- Smoothie Bowl Cafe: ingredients, toppings, nutrition powders, spoons
+- Horses: coat color, breed, age, name, owner, stall number, shoe size
+- Foraging: mushrooms, flowers, trees, bark, soil, bugs
+- Art Supplies: brushes, paints, watercolors, sketching pencils, canvases
+- Bird Watching: type, food, nest, call, wingspan, habitat
+*/
